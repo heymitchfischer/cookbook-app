1bb585c76409d1d06f195aaff7cb19bff7cf2c80
heymitchfischer <mitchfischer6@gmail.com>
2017-08-31 21:27:32 -0500

    added index, new, create

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,23 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    
+  end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    recipe.save
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# recipe = Recipe.new(
+#                     title: "Hand Sandwich",
+#                     chef: "Josh",
+#                     ingredients: "left hand, right hand, lettuce, tomato, mayo, cheese",
+#                     directions: "place ingredients on left hand, put right hand on top, enjoy"
+#                     )
+# recipe.save
+
+# recipe = Recipe.new(
+#                     title: "Garbage Lagsana",
+#                     chef: "Bad Josh",
+#                     ingredients: "old Chinese food, socks, banana peel, hot pepper",
+#                     directions: "layer ingredients in a pan, pre-heat oven to 350, cook for 5 hours, let cool"
+#                     )
+# recipe.save
