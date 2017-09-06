c8093368bfe1db456dcc1378d118a91f8cc52931
heymitchfischer <mitchfischer6@gmail.com>
2017-09-05 21:25:42 -0500

    added edit, update, destroy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,9 +15,35 @@ class RecipesController < ApplicationController
                         directions: params[:directions]
                         )
     recipe.save
+    flash[:success] = "Recipe Successfully Created"
+    redirect_to "/recipes/#{recipe.id}"
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.assign_attributes(
+                              title: params[:title],
+                              chef: params[:chef],
+                              ingredients: params[:ingredients],
+                              directions: params[:directions]
+                              )
+    recipe.save
+    flash[:info] = "Recipe Successfully Updated"
+    redirect_to "/recipes/#{recipe.id}"
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    flash[:warning] = "Recipe Successfully Destroyed"
+    redirect_to "/recipes"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   post '/recipes' => 'recipes#create'
 
   get '/recipes/:id' => 'recipes#show'
+
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' => 'recipes#update'
+  delete '/recipes/:id' => 'recipes#destroy'
 end
