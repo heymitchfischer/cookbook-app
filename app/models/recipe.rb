346704877cbfd8e64688eb480a514dc5f2f7f802
heymitchfischer <mitchfischer6@gmail.com>
2017-09-06 21:34:53 -0500

    added Bootstrap columns, etc.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,23 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%b %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result_time = ""
+    result_time += "#{hours} #{Hours.pluralize(hours)}" if hours > 0
+    result_time += ", " if hours > 0 && minutes > 0
+    result_time += "#{minutes} #{Minutes.pluralize(minutes)}" if minutes > 0
+    result_time
+  end
 end
